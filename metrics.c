4da4820d7e8f11bdef7cd8e1bdb47661003883e9
giulianobelinassi <giuliano.belinassi@usp.br>
2017-06-05 15:32:51 -0300

    Added logic for 1st case.

--- metrics.c
+++ metrics.c
@@ -27,7 +27,7 @@ int metrics_accumuled_hamming(size_t data_length, uint8_t data1[], uint8_t data2
 	return ham;
 }
 
-uint8_t* metrics_generate_modified_data(size_t data_length, uint8_t data[], int to_toggle)
+uint8_t* metrics_generate_modified_data(size_t data_length, uint8_t data[], size_t to_toggle)
 {
 	size_t i, n = data_length/8;
 	int byte, bit;
@@ -56,48 +56,60 @@ uint8_t* metrics_copy_vector(size_t length, uint8_t v[])
 	return copy;
 }
 
+#define MAX(a, b) (((a) > (b))? (a): (b))
+#define MIN(a, b) (((a) < (b))? (a): (b))
+
+
+
 void metrics_calculate_1(const char* password, size_t data_length, uint8_t data[])
 {
 	uint8_t* vector_input;
 	uint8_t* vector_input_c;
-	uint8_t* vector_modified[64];
-	uint8_t* vector_modified_c[64];
-	int* changed_bit;
+	uint8_t* vector_modified;
+	uint8_t* vector_modified_c;
 	int* ham_distance;
 	int* sum_ham;
 	int* max_ham;
 	int* min_ham;
 	
 	size_t n = data_length / 8;
-	size_t i;
+	size_t j, k, bit_to_toggle;
 
 	if (data_length % 8 != 0)
 		return -1;
 
-	changed_bit = (int*) malloc(sizeof(int)*n);
-	ham_distance         = (int*) malloc(sizeof(int)*n);
-	sum_ham              = (int*) malloc(sizeof(int)*n);
-	max_ham              = (int*) malloc(sizeof(int)*n);
-	min_ham              = (int*) malloc(sizeof(int)*n);
+	ham_distance   = (int*) malloc(sizeof(int)*n);
+	sum_ham        = (int*) calloc(n, sizeof(int));
+	max_ham        = (int*) calloc(n, sizeof(int));
+	min_ham        = (int*) calloc(n, sizeof(int));
 	
-	vector_input         = metrics_copy_vector(data_length, data);
-	vector_input_c       = k128_encrypt(password, data_length, data);
-	vector_modified      = metrics_generate_modified_data(data_length, vector_input, changed_bit);
-	vector_modified_c    = k128_encrypt(password, data_length, metrics_copy_vector(data_length, vector_modified));
+	vector_input   = metrics_copy_vector(data_length, data);
+	vector_input_c = k128_encrypt(password, data_length, data);
 
+	for (i = 0; i < n; ++i)
+		max_ham[i] = 1000;
 
 	for (i = 0; i < n; ++i)
 	{
+		for (j = 0; j < 64; ++j)
+		{
+			bit_to_toggle = 8*i + j;
+			vector_modified   = metrics_generate_modified_data(data_length, vector_input, bit_to_toggle);
+			vector_modified_c = k128_encrypt(password, data_length, metrics_copy_vector(data_length, vector_modified));
+			for (k = i; k < n; ++k)
+			{
+				int ham = metrics_hamming(((uint64_t*) vector_modified_c)[k] , ((uint64_t*)vector_input_c)[k]);
+				sum_ham[k] += ham;
+				max_ham[k] = MAX(max_ham[k], ham);
+				min_ham[k] = MIN(min_ham[k], ham);
+			}
+		}
 		
 	}
 
 	free(vector_input);
-	free(vector_input_c_k128);
 	free(vector_modified);
-	free(vector_modified_k128);
 	free(vector_modified_c);
-	free(changed_bit);
-	free(ham_distance);
 	free(sum_ham);
 	free(max_ham);
 	free(min_ham);
